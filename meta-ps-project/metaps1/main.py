7092138f72578dd9b6cb1da457bda6eed26836e5
root <denmax@lsoft.biz>
2021-07-23 11:33:40 +0300

    Добавлены недостающие файлы

---
+++ meta-ps-project/metaps1/main.py
@@ -0,0 +1,2 @@
+def execute_cmd():
+    print("test")
---
+++ meta-ps-project/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+import metaps1
+
+setup(
+    name='metaps1',
+    version=metaps1.__version__,
+    author='LSoft',
+    packages=find_packages(),
+    description='1C Enterprise installer',
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    install_requires=[],
+    entry_points={
+        'console_scripts':
+            ['meta-ps = metaps1.main:execute_cmd']
+        }
+)
