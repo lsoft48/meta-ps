c1b5e638a122f1208f9f501ffa9ee45925c9fad9
root <denmax@lsoft.biz>
2021-07-26 15:34:29 +0300

    Исправление зависимостей

--- meta-ps-project/setup.py
+++ meta-ps-project/setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 from os.path import join, dirname
 import metaps1
@@ -9,7 +10,7 @@ setup(
     packages=find_packages(),
     description='1C Enterprise installer',
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
-    install_requires=[],
+    install_requires=['requests','re', 'pprint', 'pickle],
     entry_points={
         'console_scripts':
             ['meta-ps = metaps1.main:execute_cmd']
