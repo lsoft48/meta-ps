93b33c16b39bad7707fc4480bc499b1effefeabc
root <denmax@lsoft.biz>
2021-09-02 12:46:54 +0300

    Добавлено ведение логов

--- meta-ps-project/metaps1/__init__.py
+++ meta-ps-project/metaps1/__init__.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+import logging
+
+logger = logging.getLogger(__name__)
+handeler=logging.FileHandler("/var/log/meta-ps.log", mode='w')
+handeler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(handeler)
+
+def SetLogLevel(level):
+    logger.setLevel(level)
+
 import metaps1.info as inf
 
 __version__ = inf.version
