dafc789255c80da45f222a74319da97eb46b8e00
root <denmax@lsoft.biz>
2021-09-02 12:46:04 +0300

    Исправлены зависимости

--- meta-ps-project/setup.py
+++ meta-ps-project/setup.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 from os.path import join, dirname
+import sys
 import metaps1
+
+
+reqs=['requests','patool', 'sh']
+#if sys.platform.startswith("linux"):
+#    reqs.append('python-apt')
+
 
 setup(
     name='metaps1',
@@ -10,7 +17,7 @@ setup(
     packages=find_packages(),
     description='1C Enterprise installer',
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
-    install_requires=['requests','patool'],
+    install_requires=reqs,
     entry_points={
         'console_scripts':
             ['meta-ps = metaps1.main:ExecuteCommand']
