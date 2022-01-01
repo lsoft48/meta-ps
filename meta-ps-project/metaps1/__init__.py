e843a707cce3318eefada441fa97d0c05e1a3172
denmax <denmax@lsoft.biz>
2022-01-01 19:55:36 +0300

    Исправленн вывод лога для windows

--- meta-ps-project/metaps1/__init__.py
+++ meta-ps-project/metaps1/__init__.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 import logging
+import metaps1.info as inf
 
 logger = logging.getLogger(__name__)
-handeler=logging.FileHandler("/var/log/meta-ps.log", mode='w')
+if inf.platform_linux:
+  handeler=logging.FileHandler("/var/log/meta-ps.log", mode='w')
+else:
+  handeler=logging.FileHandler("meta-ps.log", mode='w')
 handeler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 logger.addHandler(handeler)
 
 def SetLogLevel(level):
     logger.setLevel(level)
-
-import metaps1.info as inf
 
 __version__ = inf.version
 
